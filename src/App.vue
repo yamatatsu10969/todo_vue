cc6c0a27e819c84ac0fceb8572237b56fd6992b1
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-08-29 23:16:00 +0900

    Feat: base todo app ui

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,69 @@
-<script setup>
-import Title from './components/Title.vue';
-</script>
-
 <template>
-	<Title />
+	<div class="container mt-5">
+		<div class="d-flex justify-content-center">
+			<img class="logo" src="./assets/logo.svg" alt="logo" />
+			<h2 class="title">Todo App</h2>
+		</div>
+		<div class="d-flex justify-content-end">
+			<input type="text" class="form-control" placeholder="タスクを入力してください" />
+			<div class="ms-3"></div>
+			<button class="btn btn-primary">追加</button>
+		</div>
+
+		<table class="table mt-5">
+			<thead>
+				<tr>
+					<th>タスク</th>
+					<th>完了</th>
+					<th>編集</th>
+					<th>削除</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="(task, index) in tasks">
+					<td>{{ task.name }}</td>
+					<td>
+						<input type="checkbox" v-model="task.done" />
+					</td>
+					<td>
+						<span class="fa fa-trash pointer"></span>
+					</td>
+					<td>
+						<p class="fa fa-pen pointer"></p>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </template>
+
+<style>
+.header {
+	margin: auto;
+}
+.logo {
+	margin: 20px;
+	height: 30px;
+}
+.title {
+	margin: auto 0;
+}
+.pointer {
+	cursor: pointer;
+}
+</style>
+
+<script>
+export default {
+	data() {
+		return {
+			tasks: [
+				{
+					name: 'タスク１',
+					done: false,
+				},
+			],
+		};
+	},
+};
+</script>
--- src/components/Title.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<div class="container">
-		<h2 class="text-center">Todo App</h2>
-	</div>
-</template>
